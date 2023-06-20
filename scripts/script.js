64b20018bbeed4d9deefa1e6ecadffecafe23831
paulopujo <phmartinspujo18@gmail.com>
2023-06-20 09:18:35 -0300

    Site final

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const menuItems = document.querySelectorAll("nav a");
+menuItems.forEach((item)) => {
+    item.addEventListener "click", function() 
+        menuItems.forEach((item)) => {
+            item.classList.remove("active");
+        };
+    this.classList.add("active");
+};
