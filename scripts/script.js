c5f52694dbecfac458b70dec08ce5abfd1f2278a
paulopujo <phmartinspujo18@gmail.com>
2023-06-20 09:24:32 -0300

    imagens renomeadas

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const menuItems = document.querySelectorAll("nav a");
-menuItems.forEach((item)) => {
-    item.addEventListener "click", function() 
-        menuItems.forEach((item)) => {
-            item.classList.remove("active");
-        };
-    this.classList.add("active");
-};
